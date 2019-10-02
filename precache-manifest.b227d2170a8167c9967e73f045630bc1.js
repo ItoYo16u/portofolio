ad76630341c6ff5f81ee8f70a931faa7880f1636
ItoYo16u <ito.yo16uh90616@gmail.com>
2019-10-02 20:16:16 +0900

    Updates

---
+++ precache-manifest.b227d2170a8167c9967e73f045630bc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcb0454972a6f2adf4c651b18f1192be",
+    "url": "/portofolio/index.html"
+  },
+  {
+    "revision": "abcf73ff9e42c2f47293",
+    "url": "/portofolio/static/css/main.4dc71f58.chunk.css"
+  },
+  {
+    "revision": "5d76b775d174ca6b69f6",
+    "url": "/portofolio/static/js/2.18794d57.chunk.js"
+  },
+  {
+    "revision": "abcf73ff9e42c2f47293",
+    "url": "/portofolio/static/js/main.3ad1ab3b.chunk.js"
+  },
+  {
+    "revision": "316a40a210bf87b2d344",
+    "url": "/portofolio/static/js/runtime-main.2e9fb04c.js"
+  },
+  {
+    "revision": "580ffd26ccaa16481ed87c1829c74943",
+    "url": "/portofolio/static/media/ractjs.580ffd26.png"
+  },
+  {
+    "revision": "ab90f0df956a874e226662a98f1c475c",
+    "url": "/portofolio/static/media/rb.ab90f0df.png"
+  }
+]);
